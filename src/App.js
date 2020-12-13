bddb8dcd2987968147ce1f18db9c1f7c65e720b3
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-12-13 22:38:05 +0530

    added custom emojis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TextField, MuiThemeProvider } from "@material-ui/core";
+import { useState } from "react";
+import { theme } from "./theme/theme";
+
+const emojis = [
+  { emoji: "🚣", desc: "Person Rowing Boat" },
+  { emoji: "🗾", desc: "Map of Japan" },
+  { emoji: "🏔️", desc: "Snow-Capped Mountain" },
+  { emoji: "🌋", desc: "Volcano" },
+  { emoji: "🗻", desc: "Mount Fuji" },
+  { emoji: "🏥", desc: "Hospital" },
+  { emoji: "🗼", desc: "Tokyo Tower" },
+  { emoji: "🗽", desc: "Statue of Liberty" },
+  { emoji: "⛩️", desc: "Shinto Shrine" },
+];
 
 function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("Search emoji's meaning");
+
+  const onEmojiClickHandler = (inputEmoji) => {
+    setEmoji(inputEmoji.emoji);
+    setMeaning(inputEmoji.desc);
+  };
+
+  const emojiChangeHandler = (e) => {
+    const actualEmoji = e.target.value;
+    setEmoji(actualEmoji);
+    let emojiData = emojis.filter((emoji) => emoji.emoji === actualEmoji);
+    if (emojiData.length > 0) {
+      console.log(emojiData);
+      setMeaning(emojiData[0].desc);
+    } else {
+      setMeaning("emoji is not recognized");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <h2>Search Emoji</h2>
+        <div className="input">
+          <TextField
+            id="outlined-basic"
+            label="Search emoji"
+            variant="outlined"
+            value={emoji}
+            onChange={emojiChangeHandler}
+          />
+          <br />
+        </div>
+        <h3>{emoji}</h3>
+        <h3>{meaning}</h3>
+        {emojis.map((emj) => (
+          <span
+            key={emj.desc}
+            className="emoji"
+            onClick={() => onEmojiClickHandler(emj)}
+          >
+            {" "}
+            {emj.emoji}{" "}
+          </span>
+        ))}
+      </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#EEE",
+    },
+    secondary: {
+      main: "#7550E9",
+    },
+  },
+});
